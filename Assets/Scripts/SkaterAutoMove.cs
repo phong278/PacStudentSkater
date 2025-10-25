147ebe40f2ceb4480da5104859a955970ad7e73f
phong278 <phong.d.ngo@student.uts.edu.au>
2025-10-26 01:34:46 +1100

    Reapply "Added Start Screen UI with buttons, title, stats, music, and animated border"
    This reverts commit 48e79d7a6eea0f719c7ccb25014ef725fa886299.

---
+++ Assets/Scripts/SkaterAutoMove.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SkaterAutoMove : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public float speed = 3f;
+
+    private int currentWaypoint = 0;
+    private Animator animator;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>(); // grab Animator
+
+        if (audioSource != null && audioSource.clip != null)
+        {
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+
+    }
+
+    void Update()
+    {
+        if (waypoints.Length == 0) return;
+
+        Vector3 targetPos = waypoints[currentWaypoint].position;
+        Vector3 direction = (targetPos - transform.position).normalized;
+
+        // Move skater
+        transform.position += direction * speed * Time.deltaTime;
+
+        //Send direction info to Animator
+        animator.SetFloat("MoveX", direction.x);
+        animator.SetFloat("MoveY", direction.y);
+
+        // If reached waypoint, go to next
+        if (Vector3.Distance(transform.position, targetPos) < 0.05f)
+        {
+            currentWaypoint = (currentWaypoint + 1) % waypoints.Length;
+        }
+    }
+}
+
+
+
+
+
+
